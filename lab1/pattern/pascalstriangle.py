1ed52a9cc0ca134f608330de331dde63696cc151
rishita <rishita902@gmail.com>
2026-02-23 10:29:57 +0530

    pattern

---
+++ lab1/pattern/pascalstriangle.py
@@ -0,0 +1,10 @@
+n = 5
+
+for i in range(n):
+    num = 1
+    for j in range(n - i):
+        print(" ", end="")
+    for j in range(i + 1):
+        print(num, end=" ")
+        num = num * (i - j) // (j + 1)
+    print()
